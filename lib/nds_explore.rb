35b585d5f4f74cea27d9ce902a54c577b970e685
Gene Campbell III <gcampbell391@gmail.com>
2020-02-09 22:48:16 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -11,7 +11,7 @@ def print_first_directors_movie_titles
 nds = []
 nds = directors_database
 row_index = 0
-while row_index < nds.length
+while row_index < nds[0][:movies].length
 p nds[0][:movies][row_index][:title]
 end
 row_index += 1
